af446a618b17678fa0d0067bb3d1c4075719f53e
tamagoez <tabascoes0711@gmail.com>
2023-03-12 15:35:32 +0000

    Update 3 files & nookies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,26 @@
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { parseCookies, setCookie, destroyCookie } from "nookies";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const cookies = parseCookies();
+    console.log({ cookies });
+    if (!cookies.lang) {
+      setCookie(null, "lang", window.navigator.language, {
+        maxAge: 30 * 24 * 60 * 60,
+        path: "/",
+      });
+    }
+  }, []);
   return (
     <>
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
+
       <Analytics />
     </>
   );
