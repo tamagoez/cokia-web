8543d3bbc59c33e7e713a2663028b9981cd94f09
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-03-22 00:07:18 +0900

    Fix path...

--- pages/lab/konva/maineditor.tsx
+++ pages/lab/konva/maineditor.tsx
@@ -2,15 +2,27 @@ import { FC } from "react";
 import dynamic from "next/dynamic";
 
 const StageComponent = dynamic(
-  () => import("../../../components/papyrus/canvasold"),
+  () => import("../../../components/widenote/editor/page"),
   {
     ssr: false,
   }
 );
 const CanvasPage: FC = () => {
-  return <><style jsx global>{`.maincanvas {-webkit-touch-callout: none;
+  return (
+    <>
+      <style jsx global>{`.maincanvas {-webkit-touch-callout: none;
     -webkit-user-select: none;
-    user-select: none;}`}</style><div className="maincanvas" onTouchStart={(e) => {e.preventDefault()}}><StageComponent /></div></>;
+    user-select: none;}`}</style>
+      <div
+        className="maincanvas"
+        onTouchStart={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <StageComponent />
+      </div>
+    </>
+  );
 };
 
 export default CanvasPage;
