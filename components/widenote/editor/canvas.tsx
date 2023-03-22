28fbf328e619c595d8e476d642f04c5bf5eb8e66
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-03-22 09:33:32 +0900

    Update canvas.tsx

--- components/widenote/editor/canvas.tsx
+++ components/widenote/editor/canvas.tsx
@@ -1,4 +1,5 @@
 import { FC, useRef, useState } from "react";
+import Konva from "konva";
 import { Stage, Layer, Text, Line } from "react-konva";
 // import { Box } from "@chakra-ui/react";
 
@@ -16,10 +17,14 @@ export default function EditorCanvas({
   const [lines, setLines] = useState([]);
   const [tension, setTension] = useState(0.5);
   const isDrawing = useRef(false);
+  
+  const [lastCenter, setLastCenter] = useState(null);
+  const [lastDist, setLastDist] = useState(0);
+  Konva.hitOnDragEnabled = true;
 
   const handleMouseDown = (e) => {
-    if (tool === "cursor") return;
     e.evt.preventDefault();
+    if (tool === "cursor") 
     isDrawing.current = true;
     const pos = e.target.getStage().getPointerPosition();
     setLines([
@@ -29,8 +34,9 @@ export default function EditorCanvas({
   };
 
   const handleMouseMove = (e) => {
-    if (tool === "cursor") return;
     e.evt.preventDefault();
+    if (tool === "cursor") return;
+    
     // no drawing - skipping
     if (!isDrawing.current) {
       return;
@@ -48,6 +54,8 @@ export default function EditorCanvas({
 
   const handleMouseUp = () => {
     isDrawing.current = false;
+    setLastDist(0);
+    setLastCenter(null)
   };
   return (
     <>
@@ -93,4 +101,75 @@ export default function EditorCanvas({
       </Stage>
     </>
   );
+}
+
+function cursorMove(e: any, stageRef: any, lastCenter, setLastCenter, lastDist, setLastDist) {
+  function getDistance(p1, p2) {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+      }
+
+      function getCenter(p1, p2) {
+        return {
+          x: (p1.x + p2.x) / 2,
+          y: (p1.y + p2.y) / 2,
+        };
+      }
+  const stage = stageRef.current
+    const touch1 = e.evt.touches[0];
+        const touch2 = e.evt.touches[1];
+
+        if (touch1 && touch2) {
+          // if the stage was under Konva's drag&drop
+          // we need to stop it, and implement our own pan logic with two pointers
+          if (stage.isDragging()) {
+            stage.stopDrag();
+          }
+
+          var p1 = {
+            x: touch1.clientX,
+            y: touch1.clientY,
+          };
+          var p2 = {
+            x: touch2.clientX,
+            y: touch2.clientY,
+          };
+
+          if (!lastCenter) {
+            setLastCenter(getCenter(p1, p2));
+            return;
+          }
+          var newCenter = getCenter(p1, p2);
+
+          var dist = getDistance(p1, p2);
+
+          if (!lastDist) {
+            setLastDist(dist);
+          }
+
+          // local coordinates of center point
+          var pointTo = {
+            x: (newCenter.x - stage.x()) / stage.scaleX(),
+            y: (newCenter.y - stage.y()) / stage.scaleX(),
+          };
+
+          var scale = stage.scaleX() * (dist / lastDist);
+
+          stage.scaleX(scale);
+          stage.scaleY(scale);
+
+          // calculate new position of the stage
+          var dx = newCenter.x - lastCenter.x;
+          var dy = newCenter.y - lastCenter.y;
+
+          var newPos = {
+            x: newCenter.x - pointTo.x * scale + dx,
+            y: newCenter.y - pointTo.y * scale + dy,
+          };
+
+          stage.position(newPos);
+
+          setLastDist(dist);
+          setLastCenter(newCenter);
+        }
+      });
 }
