11673e383a8781374a384bcb231fc29550ee71e1
tamagoez <tabascoes0711@gmail.com>
2023-03-25 03:44:41 +0000

    Layer close fix

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -28,36 +28,6 @@ export function AlertOnClose() {
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
   }, []);
-  return;
+  return <></>;
 
-  return (
-    <>
-      <AlertDialog
-        isOpen={isOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              Delete Customer
-            </AlertDialogHeader>
-
-            <AlertDialogBody>
-              Are you sure? You can't undo this action afterwards.
-            </AlertDialogBody>
-
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button colorScheme="red" onClick={onClose} ml={3}>
-                Delete
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
-    </>
-  );
 }
